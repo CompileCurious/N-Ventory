1b11626e383c86aa716484276cff56f63efffa6d
CompileCurious <samueljsharp@protonmail.com>
2025-08-11 15:19:30 +0100

    removal of C++ packages

--- TransactionLog.cpp
+++
@@ -1,9 +0,0 @@
-void CTransactionLog::LogAction(const TDesC& action, const InventoryItem& item) {
-    TransactionEntry entry;
-    entry.timestamp.HomeTime();
-    entry.action.Copy(action);
-    entry.item = item;
-
-    if (iEntries.Count() >= 100) iEntries.Remove(0);
-    iEntries.Append(entry);
-}
