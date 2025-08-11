d1263d315cf4eb3c9cad3cad1dbd075d9b6bb0e2
CompileCurious <samueljsharp@protonmail.com>
2025-08-11 14:35:39 +0100

    Update AppUI.cpp

--- AppUI.cpp
+++ AppUI.cpp
@@ -1,3 +1,25 @@
+#include "AddEditItemDialog.h"
+#include "InventoryManager.h"
+#include "TransactionLog.h"
+
+void CNVentoryAppUi::LaunchAddItemDialogL() {
+    CAddEditItemDialog* dlg = new (ELeave) CAddEditItemDialog();
+    if (dlg->ExecuteLD(R_ADD_EDIT_ITEM_DIALOG)) {
+        iInventoryManager->AddItem(dlg->iItem);
+        iInventoryManager->SaveInventoryL();
+        iTransactionLog->LogAction(_L("Add"), dlg->iItem);
+    }
+}
+
+void CNVentoryAppUi::LaunchEditItemDialogL() {
+    InventoryItem& item = iInventoryManager->GetSelectedItem();
+    CAddEditItemDialog* dlg = new (ELeave) CAddEditItemDialog(item);
+    if (dlg->ExecuteLD(R_ADD_EDIT_ITEM_DIALOG)) {
+        iInventoryManager->UpdateItem(item, dlg->iItem);
+        iInventoryManager->SaveInventoryL();
+        iTransactionLog->LogAction(_L("Edit"), dlg->iItem);
+    }
+}
 void CNVentoryAppUi::HandleCommandL(TInt aCommand) {
     switch (aCommand) {
         case ECmdAddItem: LaunchAddItemDialogL(); break;
